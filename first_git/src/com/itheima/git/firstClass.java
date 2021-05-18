e8bf4bcd9ce2e0d0e56812128197bd10ca0387c4
wuxiang <1978395373@qq.com>
2021-05-18 20:55:59 +0800

    第一次

--- first_git/src/com/itheima/git/firstClass.java
+++ first_git/src/com/itheima/git/firstClass.java
@@ -4,4 +4,7 @@ public class firstClass {
     public static void main(String[] args) {
         System.out.printf("hello git ");
     }
+    public void test(){
+        System.out.printf("nihao");
+    }
 }
